1313c32badbeced6ec1ea3af37d5604dac9ee6ee
xiegang <gang.xie@qitantech.com>
2023-08-28 15:48:10 +0800

    test branch

--- main.cpp
+++ main.cpp
@@ -10,17 +10,6 @@
 using namespace std;
 
 int main() {
-    /*unique_ptr<LearnStl> stl = make_unique<LearnStl>();
-    unique_ptr<Sort> sort = make_unique<Sort>();
-    int arr[6] = {2,3,1,5,4,9};
-    sort->QuickSort(arr,0,6);
-    auto print_arr = [=]()-> int {
-        for (auto n : arr) {
-            std::cout << n << " " << std::endl;
-        }
-        return 0;
-    };
-    print_arr();*/
     Solution obj;
     cout << obj.GetPressedString("aaaccccccccddcccccc") << endl;
 
