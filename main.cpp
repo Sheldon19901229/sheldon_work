974b81837303cfdd85cd2b6b0f2d8c50f755070f
xiegang <black_uestc@163.com>
2023-10-31 15:48:37 +0800

    增加gitignore文件

--- main.cpp
+++ main.cpp
@@ -12,14 +12,6 @@ using namespace std;
 int main() {
     Solution obj;
     cout << obj.GetPressedString("aaaccccccccddcccccc") << endl;
-
-
-    std::cout << "ns:" << GetTimeCount(TIME_STAMP_NS) << endl;
-    std::cout << "us:" << GetTimeCount(TIME_STAMP_US) << endl;
-    std::cout << "ms:"<< GetTimeCount(TIME_STAMP_MS) << endl;
-    std::cout << "s:" << GetTimeCount(TIME_STAMP_S) << endl;
-    std::cout << "min:"<< GetTimeCount(TIME_STAMP_MIN) << endl;
-    std::cout << "hour:"<< GetTimeCount(TIME_STAMP_H) << endl;
     std::cout << GetMsTimeStamp() << endl;
     return 0;
 }
