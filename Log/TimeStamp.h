73a6808f552591d9fec7dbd7b80f46a4d4afcfc1
xiegang <674456021@qq.com>
2024-03-21 21:55:24 +0800

    complil in ubuntu

--- Log/TimeStamp.h
+++ Log/TimeStamp.h
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <string>
 #include <ctime>
+#include <cstdio>
 
 static std::string get_time_stamp_ms()
 {
@@ -13,7 +14,7 @@ static std::string get_time_stamp_ms()
     auto count = time_point.time_since_epoch().count();
     std::tm* now = gmtime(&c_time);
     char msg_buffer[64] = { 0 };
-    sprintf_s(msg_buffer, 64, "%4d-%02d-%02d %02d:%02d:%02d.%04d", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec, static_cast<int>(count % 1000));
+    sprintf(msg_buffer, "%4d-%02d-%02d %02d:%02d:%02d.%04d", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec, static_cast<int>(count % 1000));
     return std::string(msg_buffer);
 }
 
@@ -23,7 +24,7 @@ static std::string get_time_stamp_sec()
     auto c_time = std::chrono::system_clock::to_time_t(time_point);
     std::tm* now = gmtime(&c_time);
     char msg_buffer[64] = { 0 };
-    sprintf_s(msg_buffer, 64, "%4d-%02d-%02d %02d:%02d:%02d", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec);
+    sprintf(msg_buffer, "%4d-%02d-%02d %02d:%02d:%02d", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec);
     return std::string(msg_buffer);
 }
 
